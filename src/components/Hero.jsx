11c04d4c791fba2d572ecdfa70fb8a3f5fdfe41e
Ayman Chowhan <aymanchowhan@gmail.com>
2024-05-29 18:59:54 +0530

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img from '../assests/ayman_photo.png';
 import Image from 'next/image';
+import TypewriterComponent from 'typewriter-effect';
 // function calculateAge(dob) {
 //   const dobDate=new Date(dob) //Convert DOB in js at store in the variable dobDate
 // const currentDate=new Date() //Stores today date 
@@ -31,10 +32,10 @@ function Hero() {
   const age = calculateAge(dob);
   return (
 
-    <div id='home' className='h-[80vh] container m-auto flex px-[10%] '>
-      <div className='m-auto lg:justify-start lg:mx-0 lg:mb-48 mb-28 grid-cols-1 grid lg:grid-cols-2'>
-        <div className='order-1 lg:order-2 col-span-1 mt-10 lg:mt-44 flex justify-center lg:justify-start'>
-          <div className='text-center lg:text-left text-[16px]'>
+    <div id='home' className='h-[80vh] container m-auto flex px-[8%] '>
+      <div className='m-auto lg:justify-evenly lg:mb-48 mb-28 grid-cols-1 grid lg:grid-cols-2'>
+        <div className='order-1 lg:order-2 col-span-1 mt-10 lg:mt-0 flex justify-center lg:justify-start w-full'>
+          {/* <div className='text-center lg:text-left text-[16px]'>
             Hi, my name is
             <div data-aos="fade-up" className='text-center lg:text-left text-[30px] lg:text-[70px] '>
               AYMAN CHOWHAN
@@ -45,13 +46,29 @@ function Hero() {
                 </div>
               </div>
             </div>
+          </div> */}
+          <div className='my-auto lg:text-3xl text-2xl w-full text-center lg:text-left'>
+            <div className=''>Hello, My name is </div>
+            <div className='italic lg:text-7xl text-4xl font-bold lg:mb-3'>Ayman Chowhan,</div>
+            <div className='lg:flex'>
+              <div className='lg:mr-2 lg:mt-[10px]'>a <sapn className='font-bold'>{age}</sapn> years old</div>
+              <div className="lg:text-5xl text-4xl font-semibold text-[#52657d]">
+                <TypewriterComponent
+                  options={{
+                    strings: ['React Developer', 'Software Developer', 'bla bla bla'],
+                    autoStart: true,
+                    loop: true
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
         <div className=' col-span-1 flex justify-center'>
 
-        <div className=' order-2 lg:order-1 lg:w-fit my-auto flex justify-center lg:justify-end mt-8 lg:mt-18 mr-12'>
-          <Image src={img} alt='Ayman Chowhan' className='w-96 h-4/5 ' />
-        </div>
+          <div className=' order-2 lg:order-1 mt-8 lg:mt-18 w-full'>
+            <Image src={img} alt='Ayman Chowhan' className='w-2/3 mx-auto' />
+          </div>
         </div>
       </div>
     </div>
